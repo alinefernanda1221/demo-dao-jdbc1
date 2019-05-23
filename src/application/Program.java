d55820502cb5c4107b239ea4b97fb4b9d7d8cb50
alinefernanda1221 <alinefernanda1221@gmail.com>
2019-05-23 11:22:18 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
+		SimpleDateFormat simpleDate = new SimpleDateFormat("dd/MM/yyyy");
+		
 		Department obj = new Department(1, "teste");
 		System.out.println(obj);
+		
+		Seller sel = new Seller(1, "John", "john@gmail.com", new Date(), 5000.0);
+		System.out.println(sel);
 	}
 
 }
